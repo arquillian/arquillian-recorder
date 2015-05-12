68a14ee3b53be38abd68440b299bb5e4306ce7f8
Alex Soto <asotobu@gmail.com>
2015-05-12 19:49:16 +0200

    transforms ternary to if with default.

--- arquillian-recorder-screenshooter-base/arquillian-recorder-screenshooter-impl-base/src/main/java/org/arquillian/extension/recorder/screenshooter/impl/ScreenshooterLifecycleObserver.java
+++ arquillian-recorder-screenshooter-base/arquillian-recorder-screenshooter-impl-base/src/main/java/org/arquillian/extension/recorder/screenshooter/impl/ScreenshooterLifecycleObserver.java
@@ -128,12 +128,14 @@ public class ScreenshooterLifecycleObserver {
         } else {
             Class<? extends TestClass> testClass = event.getTestClass().getClass();
 
-            Class<?> annotatedClass = ReflectionUtil.getClassWithAnnotation(
-                    testClass, Blur.class);
-            return annotatedClass == null ? null
-                    : annotatedClass
-                            .getAnnotation(Blur.class).value();
+            Class<?> annotatedClass = ReflectionUtil.getClassWithAnnotation(testClass, Blur.class);
 
+            BlurLevel blurLevel = null;
+            if (annotatedClass != null) {
+                blurLevel = annotatedClass .getAnnotation(Blur.class).value();
+            }
+
+            return blurLevel;
         }
     }
 
